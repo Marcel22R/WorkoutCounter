acfd63d90fb0a54928a5f5c8cdd1530aef435b34
Marcel22R <marieder22@gmail.com>
2024-07-15 11:25:20 +0200

    First working changes, implemented a header that shows Instant.now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,9 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    buildFeatures{
+        compose=true
+    }
 
     buildTypes {
         release {
@@ -30,6 +33,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    composeOptions{
+        kotlinCompilerExtensionVersion="1.5.2"
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
@@ -40,7 +48,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.runtime.android)
+    implementation(libs.androidx.material3.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
 }
---
+++ app/src/main/java/at/bachelor/workoutcounter/MainActivity.kt
@@ -0,0 +1,36 @@
+package at.bachelor.workoutcounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+import java.util.Date
+import androidx.activity.compose.setContent
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Locale
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+            HeadlineWithDate(Date.from(Instant.now()))
+        }
+    }
+
+
+
+
+    @Composable
+    fun HeadlineWithDate(date: Date) {
+        val formattedDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(date)
+        Text(text = "Last workout: $formattedDate", color=Color.White)
+    }
+
+
+}
